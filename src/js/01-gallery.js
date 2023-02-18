24ac86fe007effe739d4c0cde9572784fdc8bed0
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-02-18 20:58:05 +0200

    added startup files

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items'; 
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+const markup = galleryItems.reduce(
+        (acc, { original, preview, description }) =>
+          (acc += `<li>
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" 
+                src="${preview}"
+                alt="${description}" />
+        </a>
+      </li>`),
+        ''
+      );
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+console.log(galleryItems);
